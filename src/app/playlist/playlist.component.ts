26fb775a4157396e3f1b0f4475474920ee728768
Gonzo Guajardo <gonzo.guajardo@gmail.com>
2017-10-12 20:34:28 -0500

    Cleaned up playlist service

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -1,25 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { Headers, Http } from '@angular/http';
-import { HttpClient } from '@angular/common/http';
-
+import { Observable } from 'rxjs/Observable';
+import { PlaylistService } from './playlist.service';
 import { Item } from './item';
 
 @Component({
     selector: 'app-playlist',
     templateUrl: './playlist.component.html',
-    styles: ['./playlist.component.css']
+    styles: ['./playlist.component.css'],
+    providers: [PlaylistService]
 })
 export class PlaylistComponent implements OnInit {
 
-    items: Item[];
+    playlist: Item[];
     audio = new Audio();
     paused = true;
 
-    constructor(private http: HttpClient) { }
+    constructor(private playlistService: PlaylistService) { }
 
     ngOnInit(): void {
-        this.http.get('http://localhost:8080/guajardo-wedding-web/api/playlist/').subscribe(data => {
-            this.items = data['items'];
+        this.playlistService.getPlaylist().subscribe((playlist: Item[]) => {
+            this.playlist = playlist;
         });
     }
 
--- src/app/playlist/playlist.service.ts
+++ src/app/playlist/playlist.service.ts
@@ -1,18 +1,38 @@
-// import { Injectable } from '@angular/core';
-// import { Headers, Http } from '@angular/http';
-// import { Observable } from 'rxjs/Observable';
+import { Injectable, OnInit } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
+import { Item } from './item';
 
-// @Injectable()
-// export class PlaylistService {
+@Injectable()
+export class PlaylistService implements OnInit {
 
-//     results: string[]
+    observablePlaylist: Observable<Item[]>;
+    playlist: Item[];
 
-//     constructor(private http: Http) {
+    constructor(private http: HttpClient) {
 
-//     }
+    }
 
-//     getPlaylist(): Observable<String> {
-//         return this.http.get('http://localhost:8080/guajardo-wedding-web/api/playlist/').subscribe(result => this.result = result.json());
-//     }
+    ngOnInit(): void {
 
-// }
+    }
+
+    getPlaylist(): Observable<Item[]> {
+        if (this.playlist) {
+            return Observable.of(this.playlist);
+        } else if (this.observablePlaylist) {
+            return this.observablePlaylist;
+        } else {
+            this.observablePlaylist = this.http.get('http://localhost:8080/guajardo-wedding-web/api/playlist/')
+                .map((response: Response) => {
+                    this.playlist = response['items'] as Item[];
+                    return this.playlist;
+                }).catch(error => {
+                    return Observable.throw(new Item());
+                }).share();
+            return this.observablePlaylist;
+        }
+    }
+
+}
