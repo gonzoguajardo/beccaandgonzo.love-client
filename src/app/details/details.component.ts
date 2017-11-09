74c16f80a0193248cac01db7864c26d73ed162d8
Gonzo <gonzo.guajardo@gmail.com>
2017-11-09 17:54:42 -0600

    google map support

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -7,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
+  loading = true;
+  googleMapSource = 'https://www.google.com/maps/embed/v1/directions?key=AIzaSyDKdrs1gTYdi-b5IBRqJdnCpguIZ_SF8y8'
+  // + '&origin=Miami+International+Airport&destination=Briza+on+the+Bay+1717+N+Bayshore+Dr+Miami+FL+33132';
+  + '&q=Space+Needle+Seattle+WA';
+
   constructor() { }
 
   ngOnInit() {
 
+  }
+
+  load() {
+    this.loading = false;
   }
 
 }
