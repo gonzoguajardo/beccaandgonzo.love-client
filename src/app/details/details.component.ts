0a89aa774596f13529558a945f1b794864bac643
Gonzo <gonzo.guajardo@gmail.com>
2017-11-29 18:59:56 -0600

    Add dynamic styling for details page

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -9,12 +9,26 @@ export class DetailsComponent implements OnInit {
 
   loading = true;
   googleMapSource = 'https://www.google.com/maps/embed/v1/directions?key=AIzaSyDKdrs1gTYdi-b5IBRqJdnCpguIZ_SF8y8'
-  // + '&origin=Miami+International+Airport&destination=Briza+on+the+Bay+1717+N+Bayshore+Dr+Miami+FL+33132';
-  + '&q=Space+Needle+Seattle+WA';
+    + '&q=Space+Needle+Seattle+WA';
+  dateOfWedding = new Date(1549753200 * 1000);
+  fonts: string[];
+
+  dateFontSize = 350;
+  dateFontFamily = 'Abril Fatface';
+
+  headerFontSize = 250;
+  headerFontFamily = 'Abril Fatface';
+
+  paragraphFontSize = 150;
+  paragraphFontFamily = 'Abril Fatface';
 
   constructor() { }
 
   ngOnInit() {
+
+    this.fonts = ['Abril Fatface'
+      , 'Crete Round'
+    ];
 
   }
 
