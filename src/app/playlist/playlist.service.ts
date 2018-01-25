88ed3063afc7f0bfdb35fcad3c6fce4deb266b61
Gonzo <gonzo.guajardo@gmail.com>
2018-01-24 21:50:52 -0600

    Error handling when server is down, scale image in our-story

--- src/app/playlist/playlist.service.ts
+++ src/app/playlist/playlist.service.ts
@@ -8,6 +8,11 @@ import { Album } from './album';
 import { Image } from './image';
 import { Artist } from './artist';
 import { Playlist } from './playlist';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/share';
+import 'rxjs/add/observable/throw';
+// import 'rxjs/Rx';
 
 @Injectable()
 export class PlaylistService implements OnInit {
