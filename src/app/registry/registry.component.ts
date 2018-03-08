6c9637188e95334fbf2319d38142d5033cd484f5
Gonzo <gonzo.guajardo@gmail.com>
2018-03-07 19:45:01 -0600

    Fill in registry component details
    * Address bug where multiple divs were being created when swtiching between hamburger and regular menu.

--- src/app/registry/registry.component.ts
+++ src/app/registry/registry.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderService } from '../header/header.service';
+import { HeaderTitles, Header } from '../header/header';
 
 @Component({
   selector: 'app-registry',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistryComponent implements OnInit {
 
-  constructor() { }
+  // tslint:disable-next-line:max-line-length
+  readonly bedBathLink = 'https://www.bedbathandbeyond.com/store/giftregistry/view_registry_guest.jsp?pwsToken=&eventType=Wedding&inventoryCallEnabled=true&registryId=545458868&pwsurl=';
+
+  headers: Header[];
+
+  constructor(headerService: HeaderService) {
+    headerService.activateItem(HeaderTitles.REGISTRY);
+    headerService.getHeaders().subscribe((headers: Header[]) => {
+      this.headers = headers;
+    });
+  }
 
   ngOnInit() {
+  }
+
+  bedBatchClick() {
+    window.open(this.bedBathLink);
   }
 
 }
